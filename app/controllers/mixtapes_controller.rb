7dc8147086dd0d0a2a3575c23375306afe05e4e1
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-08-14 10:06:46 -0400

    gem file install pry for trouble shooting needs

--- app/controllers/mixtapes_controller.rb
+++ app/controllers/mixtapes_controller.rb
@@ -8,11 +8,40 @@ class MixtapesController < ApplicationController
     end
 
 
+    def update
+
+        mixtape = Mixtape.find(params[:id])
+
+        if mixtape.update(mixtape_params)
+            render json: Mixtape.all.to_json(:include => :opinions)
+        else
+            render json: {error: "This wasnt good, try again"}
+        end
+
+
+    end
+
+    # def create
+        
+
+
+    # end
+
+    # def destroy
+
+
+    # end
 
 
 
 
+    private 
 
-    
+    def mixtape_params
+
+        params.require(:mixtape).permit(:title, :description, :likes, :artist, :genre, :dislikes)
+
+    end
+
 
 end
